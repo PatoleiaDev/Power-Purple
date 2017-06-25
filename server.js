30766b7db5eb4d4268e3bebdb6798c45f8464b8f
Your Name <valentinomp95@icloud.com>
2017-06-25 11:19:00 +0100

    Beta version

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index.hbs');
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
